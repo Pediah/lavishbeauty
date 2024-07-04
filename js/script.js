80f8b36971200066b0dc01a92f0cd0b9a503c861
unknown <paulpmeck@gmail.com>
2024-07-04 22:26:38 +0200

    gallery

--- js/script.js
+++ js/script.js
@@ -115,3 +115,35 @@ document.addEventListener("DOMContentLoaded", function() {
         lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
     });
 });
+
+// gallery js
+
+let slideIndex = 1;
+
+function openLightbox() {
+    document.getElementById('lightbox').style.display = 'block';
+    showSlides(slideIndex);
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('slide');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
